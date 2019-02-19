f09a6d0207b0ce62eab7d867cff742822c45f0cb
浅忆梦微凉 <3538922590@qq.com>
2019-02-19 16:14:09 +0800

    滑动窗口

--- 判断一个数经过几步可以变成菲波那切数/test.cpp
+++ 判断一个数经过几步可以变成菲波那切数/test.cpp
@@ -100,69 +100,111 @@
 //	return 0;
 //}
 
+//#include<iostream>
+//#include<vector>
+//
+//using namespace std;
+//
+//int Solution(int cow_num, vector<int> apple){
+//	if (apple.empty()){
+//		return -1;
+//	}
+//	if (apple.size() == 1){
+//		return 0;
+//	}
+//	int sum = 0;
+//	for (size_t i = 0; i<apple.size(); ++i){
+//		sum += apple[i];
+//	}
+//	int count = 0;
+//	int average = sum / cow_num;
+//	if (sum%cow_num != 0){
+//		return -1;
+//	}
+//
+//	for (int i = 0; i<apple.size(); ++i){
+//		if (apple[i] == average){
+//			continue;
+//		}
+//		if (apple[i]>average){
+//			while (apple[i]>average){
+//				apple[i] -= 2;
+//				++count;
+//			}
+//			if (apple[i] != average){
+//				return -1;
+//			}
+//		}
+//		else{
+//			while (apple[i]<average){
+//				apple[i] += 2;
+//				++count;
+//			}
+//			if (apple[i] != average){
+//				return -1;
+//			}
+//		}
+//	}
+//	return count / 2;
+//}
+//
+//
+//int main(){
+//	int cow_num = 0;
+//	cin >> cow_num;
+//	int i = 0;
+//	vector<int> apple;
+//	apple.resize(cow_num);
+//	while (i<cow_num){
+//		int input = 0;
+//		cin >> input;
+//		apple[i] = input;
+//		++i;
+//	}
+//	int temp = 0;
+//	temp = Solution(cow_num, apple);
+//	cout << temp;
+//	return 0;
+//}
+
+
 #include<iostream>
 #include<vector>
 
 using namespace std;
 
-int Solution(int cow_num, vector<int> apple){
-	if (apple.empty()){
-		return -1;
-	}
-	if (apple.size() == 1){
-		return 0;
-	}
-	int sum = 0;
-	for (size_t i = 0; i<apple.size(); ++i){
-		sum += apple[i];
-	}
-	int count = 0;
-	int average = sum / cow_num;
-	if (sum%cow_num != 0){
-		return -1;
-	}
 
-	for (int i = 0; i<apple.size(); ++i){
-		if (apple[i] == average){
-			continue;
+class Solution{
+public:
+	int Themaxsum(vector<int> array){
+		if (array.empty()){
+			return 0;
 		}
-		if (apple[i]>average){
-			while (apple[i]>average){
-				apple[i] -= 2;
-				++count;
+		int i = 0;
+		int j = 1;
+		int sum = array[i];
+		while (j < array.size()){
+			int temp = i;
+			int count = 0;
+			for (; temp < j; ++temp){
+				count += array[temp];
 			}
-			if (apple[i] != average){
-				return -1;
+			if (count>sum){
+				sum = count;
 			}
+			if (count < sum){
+				++i;
+			}
+			j++;
 		}
-		else{
-			while (apple[i]<average){
-				apple[i] += 2;
-				++count;
-			}
-			if (apple[i] != average){
-				return -1;
-			}
-		}
+		return sum;
 	}
-	return count / 2;
-}
-
+};
 
 int main(){
-	int cow_num = 0;
-	cin >> cow_num;
-	int i = 0;
-	vector<int> apple;
-	apple.resize(cow_num);
-	while (i<cow_num){
-		int input = 0;
-		cin >> input;
-		apple[i] = input;
-		++i;
-	}
-	int temp = 0;
-	temp = Solution(cow_num, apple);
-	cout << temp;
+	vector<int> array = { -1, 2, 4, -5, 5, 15, 6 };
+	Solution s;
+	cout << s.Themaxsum(array);
+	system("pause");
 	return 0;
 }
